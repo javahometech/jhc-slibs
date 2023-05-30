5a49fd750e685667d1403ae8e81e9bd5eefa05e5
javahometech <hari.kammana@gmail.com>
2023-05-30 08:22:09 +0530

    added new variable to take war file name

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,6 +1,6 @@
-def call(user,ip,credId){
+def call(user,ip,credId,warName){
   sshagent([credId]) {
-    sh "scp -o StrictHostKeyChecking=no target/hr-api.war ${user}@${ip}:/opt/tomcat9/webapps/"
+    sh "scp -o StrictHostKeyChecking=no target/${warName}.war ${user}@${ip}:/opt/tomcat9/webapps/"
     sh "ssh ${user}@${ip} /opt/tomcat9/bin/shutdown.sh"
       sh "ssh ${user}@${ip} /opt/tomcat9/bin/startup.sh"
     }
